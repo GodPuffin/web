ced0a7bc0f0d6633b9e89fff5fe90d8676e3b696
Marcus Lee <57817415+GodPuffin@users.noreply.github.com>
2023-02-07 20:11:17 -0500

    Fix Syntax Highlighting

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
+const { remarkCodeHike } = require("@code-hike/mdx")
+const theme = require("shiki/themes/material-palenight.json")
+
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js',
+  mdxOptions: {
+    remarkPlugins: [[remarkCodeHike, { theme }]],
+  },
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
 module.exports = withNextra()
